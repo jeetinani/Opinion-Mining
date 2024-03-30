26e6b0648f135c5916027271a01dd382e372ab80
Jeet Inani <jeetinani@gmail.com>
2024-03-30 22:15:03 +0530

    fixing vectorizer not fitted issue

--- models/Model.py
+++ models/Model.py
@@ -5,22 +5,27 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from helpers.Transformer import getTransformedReview
 
+dataSet = pd.read_csv('model-restaurant - restaurant.csv')
+
 def loadVectorizer():
     return TfidfVectorizer(max_features=15000)
 
 tfidfVectorizer = loadVectorizer()
 
 def loadModel():
+    #moving this above model check to ensure vectorizer is always fitted.
+    corpus=[]
+    for review in dataSet['reviews']:
+        corpus.append(getTransformedReview(review))
+    X=tfidfVectorizer.fit_transform(corpus).toarray()
+    
     filePath = 'models/model.pkl'
     file=Path(filePath)
     if(file.exists()):
         print ("loading from existing model")
         return pickle.load(open(filePath,'rb'))
-    dataSet = pd.read_csv('model-restaurant - restaurant.csv')
-    corpus=[]
-    for review in dataSet['reviews']:
-        corpus.append(getTransformedReview(review))
-    X=tfidfVectorizer.fit_transform(corpus).toarray()
+
+
     y=dataSet.iloc[:,1].values
     miner=MultinomialNB().fit(X,y)
     print("new model created")
